c267421ffc8640967994c44891c50c2931f0e101
Victor Okpon <vikzy2rhyme@yahoo.com>
2021-04-08 18:11:49 +0100

    Fix CORS updated

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ let logger = require("morgan"),
 
 // CORS CONFIGURATION CONTROL
 // Set headers to prevent cors error.
+//
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
